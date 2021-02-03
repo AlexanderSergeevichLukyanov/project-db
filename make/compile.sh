e5c3d02a59928b15bf5bde97a10c487ece5a4816
alexluk4723 <71816679+alexluk4723@users.noreply.github.com>
2021-02-03 23:50:34 +0300

    Update compile.sh

--- make/compile.sh
+++ make/compile.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-g++ src/tests/*.cpp mytest/src/mytest.cpp -I include -I mytest/include -o dbms
+g++ src/tests/*.cpp mytest/src/mytest.cpp  mytest/src/mytest_main.cpp -I include -I mytest/include -o dbms
