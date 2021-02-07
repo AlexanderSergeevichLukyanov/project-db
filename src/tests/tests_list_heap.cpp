f2f7065f678cfa4125357a0de6001f766ff7086f
alexluk4723 <71816679+alexluk4723@users.noreply.github.com>
2021-02-07 22:57:13 +0300

    Update tests_list_heap.cpp

--- src/tests/tests_list_heap.cpp
+++ src/tests/tests_list_heap.cpp
@@ -1,52 +1,180 @@
 #include "list_heap.h"
 #include "mytest.h"
+#include <algorithm>
+//#define list_wrong_test
+#define list_construct
+#define list_insert
+#define list_get_min
+//#define list_make
+#define list_extract_min
+//#define list_merge
+//#define list_solyanka
+#define list_construct_comp
+#define list_insert_comp
+#define list_get_min_comp
+//#define list_make_comp
+#define list_extract_min_comp
+//#define list_merge_comp
+//#define list_solyanka_comp
+namespace{
+struct CloserTo {
+    private:
+        int center_;
 
-TEST_CASE("Testing GetMin()") {
-    list_heap<int> x;
-    for (int i = 0; i < 1000; ++i) {
-        x.push(i);
-    }
-    CHECK(x.top() == 0);
+    public:
+        explicit CloserTo(int center) : center_(center) {
+        }
+        CloserTo(const CloserTo &) = default;
+        CloserTo(CloserTo &&) = default;
+        CloserTo &operator=(const CloserTo &) = delete;
+        CloserTo &operator=(CloserTo &&) = delete;
+        ~CloserTo() = default;
+        bool operator()(int a, int b) const {
+            return std::abs(a - center_) < std::abs(b - center_);
+        }
+};
+
+#ifdef list_wrong_test	
+TEST_CASE("list-heap: demo-test"){
+	CHECK(2*2==5);
+}
+#endif
+
+#ifdef list_construct
+TEST_CASE("list-heap: constructors without Compare"){
+	list_heap<int> h1;
+	list_heap<double> h2;
+	list_heap<std::string> h3;
+	//only compile
+}
+#endif
+
+#ifdef list_insert
+TEST_CASE("list-heap: 30000 insert"){
+	list_heap<int> h1;
+	for(int i=0; i<30000; ++i){
+		h1.insert(rand());
+	}
+	CHECK(h1.size()==30000);
 }
 
-TEST_CASE("Testing pop()") {
-    list_heap<int> x;
-    x.push(1);
-    x.push(-2);
-    x.push(1000);
-    CHECK(x.top() == -2);
-    x.pop();
-    CHECK(x.top() == 1);
+TEST_CASE("list-heap: 300000 insert"){
+	list_heap<int> h1;
+	for(int i=0; i<300000; ++i){
+		h1.insert(rand());
+	}
+	CHECK(h1.size()==300000);
+}
+#endif
+
+#ifdef list_get_min
+TEST_CASE("list-heap: GetMin()"){
+	list_heap<int> h1;
+	int min=300000;
+	for(int i=0; i<300000; ++i){
+		int x = rand()%300000;
+		if(x<min) min=x;
+		h1.insert(x);
+	}
+	CHECK_MESSAGE(h1.getMin()==min, "generate min is other");
 }
 
-TEST_CASE("Testing size()") {
-    list_heap<int> x;
-    for (int i = 0; i < 10000; ++i) {
-        x.push(i);
-    }
-    CHECK_TIME("AFTER 10000 PUSH");
-    CHECK(x.size() == 10000);
-    for (int i = 0; i < 10000; ++i) {
-        x.pop();
-    }
-    CHECK(x.size() == 0);
-    //CHECK(!x.empty());  // WRONG TEST!!!!!!!!!!!!!!!!!!!!!!!
+TEST_CASE("list-heap: 300000 GetMin()"){
+	list_heap<int> h1;
+	int min=300000;
+	for(int i=0; i<300000; ++i){
+		int x = rand()%300000;
+		if(x<min) min=x;
+		h1.insert(x);
+		CHECK_MESSAGE(h1.getMin()==min, "generate min is other");
+		if(h1.getMin()!=min) break;
+	}
+}
+#endif
+
+#ifdef list_extract_min
+TEST_CASE("list-heap: 30000 insert and 30000 extract_min"){
+	list_heap<int> h1;
+	vector<int> res;
+	for(int i=0; i<30000; ++i){
+		int x = rand()%30000;
+		h1.insert(x);
+		h1.push_back(x);
+	}
+	sort(res, res.begin(), res.end());
+	CHECK_TIME("After 30000 insert random  and sort random x, then start checking extract_min");
+	for(int i=0; i<30000; ++i){
+		int x = rand()%30000;
+		CHECK(h1.getMin()==res[i]);
+		h1.extractMin();
+	}
 }
 
-TEST_CASE("Testing merge(other)") {
-    list_heap<int> x;
-    for (int i = 0; i < 100000; ++i) {
-        x.push(i);
-    }
-    CHECK_TIME("AFTER 100000 PUSH");
-    list_heap<int> y;
-    y.push(2);
-  /*  CHECK_MESSAGE(
-        y.size() == 3,
-        "Size of this list_heap is not 3");  // WRONG
-                                             // TEST!!!!!!!!!!!!!!!!!!!!!!!
-                                             */
-    y.merge(x);
-    CHECK_TIME("AFTER merge");
-    CHECK(y.size() == 100001);
+TEST_CASE("list-heap: 300000 insert and 300000 extract_min"){
+	list_heap<int> h1;
+	vector<int> res;
+	for(int i=0; i<300000; ++i){
+		int x = rand()%300000;
+		h1.insert(x);
+		h1.push_back(x);
+	}
+	sort(res, res.begin(), res.end());
+	CHECK_TIME("After 300000 insert random  and sort random x, then start checking extract_min");
+	for(int i=0; i<300000; ++i){
+		int x = rand()%300000;
+		CHECK(h1.getMin()==res[i]);
+		h1.extractMin();
+	}
+}
+#endif
+
+#ifdef list_make
+TEST_CASE("list-heap: make (30000 elements)"){
+	int arr[30000];
+	int min=30000;
+	for(int i=0; i<30000; ++i){
+		int x = rand()%30000;
+		if(min>x) min=x;
+	}
+	h1.make(arr);
+	CHECK_MESSAGE(h1.size()==30000, "size heap must be equal to the array size");
+	CHECK_MESSAGE(h1.getMin()==min, "min heap must be equal to the array min");
+}
+TEST_CASE("list-heap: make (300000 elements)"){
+	int arr[300000];
+	int min=300000;
+	for(int i=0; i<300000; ++i){
+		int x = rand()%300000;
+		if(min>x) min=x;
+	}
+	h1.make(arr);
+	CHECK_MESSAGE(h1.size()==300000, "size heap must be equal to the array size");
+	CHECK_MESSAGE(h1.getMin()==min, "min heap must be equal to the array min");
+}
+#endif
+
+#ifdef list_constructwithcomp 
+TEST_CASE("list-heap: constructors with Compare"){
+    list_heap<int, CloserTo> heap(CloserTo(10));
+	//only compile
+}
+#endif
+
+#ifdef list_insertwithcomp
+TEST_CASE("list-heap-with-compare: 30000 insert"){
+	list_heap<int, CloserTo> h1(CloserTo(10));
+	for(int i=0; i<30000; ++i){
+		h1.insert(rand());
+	}
+	CHECK(h1.size()==30000);
+}
+
+TEST_CASE("list-heap: 300000 insert"){
+	list_heap<int CloserTo> h1(CloserTo(10));
+	for(int i=0; i<300000; ++i){
+		h1.insert(rand());
+	}
+	CHECK(h1.size()==300000);
+}
+#endif
 }
