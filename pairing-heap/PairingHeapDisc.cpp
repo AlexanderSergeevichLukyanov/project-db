3718d5f1711b65c0ed1136c0c524e4f8d9f839a5
alexluk4723 <71816679+alexluk4723@users.noreply.github.com>
2021-04-11 02:21:43 +0300

    upgrade PH 11.04.2021

--- pairing-heap/PairingHeapDisc.cpp
+++ pairing-heap/PairingHeapDisc.cpp
@@ -12,13 +12,18 @@ private:
 		
 	}
 	
-	void buf_to_block(buffer<T, BlockSize, Compare> &to){
-		while(!buf.empty() && to.size()<BlockSize){
-			to.insert(buf.getMax()); //TODO!
+	void buf_to_block(buffer<T, BlockSize, Compare> &to, Head &h){ // ссылки не привязывает(
+		while(to.size()<BlockSize){ //buf точно не кончится, иначе не сливаем его в блок, в нем должно быть BS+3
+			to.insert(buf.getMax());
 			buf.extractMax();
 		}
+		h.e3=buf.getMax();
+		buf.extractMax();
+		h.e2=buf.getMax();
+		buf.extractMax();
+		h.e1=buf.getMax();
+		buf.extractMax();
 		//TODO!!! HEAD! Не здесь, правда
-		
 	}
 	
 	void block_to_buf(buffer<T, BlockSize, Compare> &from){
@@ -42,7 +47,77 @@ private:
 	
 	
 public:
+
+	[[nodiscard]] bool empty() const{
+		return (heads_of_blocks.empty() && buf.empty());
+	}
 	
+	[[nodiscard]] size_t size() const{
+		return (heads_of_blocks.size() + buf.size());
+	}
+	
+	void insert(T &x){
+		buf.insert(x);
+		if(buf.size()>..){
+			//TODO: псевдокод
+			new block new_bl;
+			new Head h;
+			buf_to_block(new_bl, h){
+			}
+			link(h, new_bl);
+			WRITE("dn", "..", new_bl);
+		}
+	}
+	
+	const T& getMin() const{
+		//TODO: бросить исключение на пустые
+		if(buf.empty()){
+			return heads_of_blocks.getMin().n1;
+		}
+		
+		if(heads_of_blocks.empty()){
+			return buf.n1;
+		}
+		
+		if(comp(buf.getMin(), heads_of_blocks.getMin().n1){
+			return buf.getMin();
+		} else{
+			return heads_of_blocks.getMin().n1;
+		}
+	}
+	
+	void extractMin(){
+		
+		if(buf.empty()){
+			Head h = heads_of_blocks.getMin();
+			heads_of_blocks.extractMin();
+			h.n1=h.n2;
+			h.n2=h.n3;
+			h.n3=NULL;
+			--h.size;
+			if(h.empty()){
+				//psevdo
+				READ;
+				block_to_buf();
+			} else{ 
+				//todo: пополнение из буфера
+				heads_of_blocks.insert(h);
+			}
+		//	return heads_of_blocks.getMin().n1;
+		}
+		
+		if(heads_of_blocks.empty()){
+			buf.extractMin();
+			//return buf.n1;
+		}
+		
+		if(comp(buf.getMin(), heads_of_blocks.getMin().n1){
+			buf.extractMin();
+		} else{
+			//todo: что и в первом случае
+			//return heads_of_blocks.getMin().n1;
+		}
+	}
 	/** TODO: тут еще поколдовать надо над методами, они тривиальны, просто куча + буффер и немного алгоритмической магии) */
 }
 
@@ -83,12 +158,12 @@ public:
 		heap_of_del_elements.insert(x);
 	}
 	
-	void extractMin(){
+	void extractMin(){ // pop
 		flush_dels();
 		heap_of_elements.extractMin();
 	}
 	
-	const T& getMin() const{
+	const T& getMin() const{ // top
 		flush_dels();
 		return heap_of_elements.getMin();
 	}
