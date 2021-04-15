99712631e0f08a878c05918d147aed425b8a93c2
alexluk4723 <71816679+alexluk4723@users.noreply.github.com>
2021-04-15 22:32:20 +0300

    add phwb tests

--- include/heaps_with_buf/ph_with_buffer.h
+++ include/heaps_with_buf/ph_with_buffer.h
@@ -1,17 +1,7 @@
+#include "buffer.h"
+#include "head.h"
+#include "block.h"
 #include "pairing_heap.h"
-#include "data_str/buffer.h"
-#include "data_str/block.h"
-#include "data_str/head.h"
-const size_t B = 32768; // в будущем это в конфиг-файле
-
-//using namespace EMHS;
-
-const std::string folder_name = "ph-data";
-std::size_t block_counter = 0;
-
-
-namespace {
-
 template <typename T, size_t BlockSize, typename Compare=std::less<T>>
 struct pairing_heap_with_buffer{
 private:
@@ -149,59 +139,4 @@ public:
 			}
 		}
 	} //вроде done
-}
-}// anonymus namespace end
-
-
-template <typename T, typename Compare =  std::less<T>, size_t BlockSize=B/sizeof(T)> 
-struct PairingHeap{
-private:
-	Compare comp;
-
-	::pairing_heap_with_buffer<T, Compare> heap_of_elements; //обычные
-	::pairing_heap_with_buffer<T, Compare> heap_of_del_elements; //удалённые
-	
-	void flush_dels(){ //убираем удаленные, пока он сравнимы с top
-		if(!heap_of_del_elements().empty() && !heap_of_del_elements.empty() && heap_of_del_elements.getMin() == heap_of_elements.getMin()){ 
-			heap_of_del_elements.extractMin(); //убрали
-			heap_of_elements.extractMin(); //убрали
-			flush_dels(); // рекусивно продолжаем убиарть всё это
-		} //обе не пусты и равны минимумы
-	}
-
-public:
-
-	PairingHeap() = default;
-	
-	
-	[[nodiscard]] bool empty() const { 
-		return (heap_of_elements.size() == heap_of_del_elements.size()); //количество удаленных равно количеству добавленных => пусто
-	}
-	
-	[[nodiscard]] size_t size() const { 
-		return heap_of_del_elements.size() - heap_of_del_elements.size(); //количество добавленных мину количество удаленных
-	}
-	
-	void add(const T& x) {
-		heap_of_elements.insert(x);
-	}
-	
-	void del(const T& x) {
-		heap_of_del_elements.insert(x);
-	}
-	
-	void extract() { 
-		flush_dels();
-		heap_of_elements.extractMin();
-	}
-	
-	const T& top() const {
-		flush_dels();
-		return heap_of_elements.getMin();
-	}
-	
-	void decreaseKey(T& x, T& new_x){
-		heap_of_del_elements.insert(x);
-		heap_of_elements.insert(new_x);
-	}
 }
