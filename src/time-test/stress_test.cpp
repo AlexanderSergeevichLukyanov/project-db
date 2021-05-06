992f2122e2ec7f18f77baa8a6413e51983519043
alexluk4723 <71816679+alexluk4723@users.noreply.github.com>
2021-05-06 16:20:02 +0300

    test BigData

--- src/time-test/stress_test.cpp
+++ src/time-test/stress_test.cpp
@@ -51,8 +51,8 @@ void extract_mins(int count, float &time_, T &test_str){
 
 
 int main(){
-	std::cerr<<"\x1b[33mAfter 100'000'000 insert():\n\x1b[0m";
-	const int insert_count=100'000'000;
+	std::cerr<<"\x1b[33mAfter 10'000'000 insert():\n\x1b[0m";
+	const int insert_count=10'000'000;
 	{
 	#ifdef list_heap_
 	list_heap<int> h;
