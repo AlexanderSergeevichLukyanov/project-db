01419c606f2d2c84fd43d3b4016662eec4db5594
alexluk4723 <71816679+alexluk4723@users.noreply.github.com>
2021-05-28 18:03:24 +0300

    def_disk

--- lib/include/data_str/default_disk_rw.h
+++ lib/include/data_str/default_disk_rw.h
@@ -15,7 +15,7 @@ namespace EMHS{
     public:
         DISK() = default;
 
-    void WRITE_DISK(char * filename, char * data, std::size_t len){
+    void WRITE_DISK(const char * filename, char * data, std::size_t len){
         int file_descriptor = open64(filename, O_RDWR|O_CREAT, 0777);
         open_descriptors[std::string(filename)] = file_descriptor;
         if(file_descriptor == -1){
@@ -30,7 +30,7 @@ namespace EMHS{
         fdatasync(file_descriptor);
         
     }
-    void READ_DISK(char * filename, char * data, std::size_t len){
+    void READ_DISK(const char * filename, char * data, std::size_t len){
         int file_descriptor = open_descriptors[std::string(filename)];
         if(file_descriptor == -1){
             std::cerr << "Ошибка при открытии/создании файла\n";
@@ -73,7 +73,7 @@ template<typename T>
     void READ(uint64_t NextWrite, EMHS::Block_t<T> & Block){
        // std::unique_ptr<FILE, decltype(& fclose)> File(fopen(NameMake(NextWrite).c_str(), "rb"), & fclose);
        // fread(& Block[0], sizeof(T), Block.capacity(), File.get());
-        EMHS::d.READ_DISK(NameMake(NextWrite).c_str(), & Block[0], sizeof(T)*Block.capacity());
+        EMHS::d.READ_DISK(NameMake(NextWrite).c_str(), (char*)&Block[0], sizeof(T)*Block.capacity());
         I_COUNTER++;
     }
     
@@ -82,7 +82,7 @@ template<typename T>
         //std::cerr<<NameMake(NextWrite);
         //std::unique_ptr<FILE, decltype(& fclose)> File(fopen(NameMake(NextWrite).c_str(), "wb"), & fclose);
         //fwrite(& Block[0], sizeof(T), Block.capacity(), File.get());
-        EMHS::d.WRITE_DISK(NameMake(NextWrite).c_str(), & Block[0], sizeof(T)*Block.capacity());
+        EMHS::d.WRITE_DISK(NameMake(NextWrite).c_str(), (char*)&Block[0], sizeof(T)*Block.capacity());
         O_COUNTER++;
     }
 
